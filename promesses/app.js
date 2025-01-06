14c59887f152e469ee75c61a6dcee977bfc1d5ed
michelint <lemoderateurdelive@gmail.com>
2025-01-06 10:22:28 +0100

    Maj : Test Séquentiel

--- promesses/app.js
+++ promesses/app.js
@@ -1,1 +1,11 @@
-console.log("caca")
+console.log(1)
+console.log(2)
+
+
+setTimeout(()=>{ // Simuler prendre 3s
+    console.log("Fonction de callback appelée")
+},3000)
+
+
+console.log(3)
+console.log(4)
