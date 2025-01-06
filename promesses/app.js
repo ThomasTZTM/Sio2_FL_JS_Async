65763642b38f312cdd2883c0bd79f64a64bf4723
michelint <lemoderateurdelive@gmail.com>
2025-01-06 10:45:58 +0100

    Maj : Test Async

--- promesses/app.js
+++ promesses/app.js
@@ -1,18 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// PROMESSE //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 let obtenirBTS = new Promise( (resolve,reject) => {
     // Passer le BTS et obtenir le result
 
     // Simuler le fait que le résultat est arrivé et favorable
-    let btsObtenu = false
-
-    if (btsObtenu) {
-        // La promesse a été tenue
-        resolve("BTS Obtenu")
-    }else{
-        // La promesse n'a pas été tenue
-        reject("BTS Non obtenu")
-    }
-
+    setTimeout( ()=> {
+        let btsObtenu = true
+        if (btsObtenu) {
+            // La promesse a été tenue
+            resolve("BTS Obtenu")
+        }else{
+            // La promesse n'a pas été tenue
+            reject("BTS Non obtenu")
+        }
+    }, 5000)
 })
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// INSTRUCTION //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+console.log(1)
 
 obtenirBTS
     .then((resultat) => { // then = promesse tenue
@@ -21,3 +31,5 @@ obtenirBTS
     .catch((erreur) => { // catch = promesse non tenue
         console.log(erreur)
     })
+
+console.log(2)
