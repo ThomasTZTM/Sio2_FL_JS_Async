b3a6a307bc516ac976ae401f31058e43ac12a551
michelint <lemoderateurdelive@gmail.com>
2025-01-07 10:20:26 +0100

    Maj : Affichage des noms avec l'API JsonPlaceHolder

--- tp1/app.js
+++ tp1/app.js
@@ -1,10 +1,42 @@
 // Appel à un endPoint ( jsonplaceholder )
 // Utiliser fetch
-
+console.log("test")
 fetch("https://jsonplaceholder.typicode.com/users")
     .then( (reponse) => {
-        console.log(reponse)
+        return reponse.json()
     })
+        .then( (users) => {
+
+            console.log(users)
+
+            // Afficher le nom de chaque user dans la page HTML
+            const usersList = document.querySelector("#users-list")
+            console.log(usersList)
+            // Afficher chaque utilisateur dans <p>
+            usersList.innerHTML = users
+                .map( (user)=>`<p>${user.name}</p>` )
+                .join(' ')
+
+        })
     .catch( (erreur) => {
         console.log(erreur)
     })
+
+// Faire la meme chose avec async et await
+
+const fetchUsers = async  () => {
+    try {
+        const reponse = await fetch()
+        const users = await reponse.json()
+        // Afficher le nom de chaque user dans la page HTML
+        const usersList = document.querySelector("#users-list")
+        console.log(usersList)
+        // Afficher chaque utilisateur dans <p>
+        usersList.innerHTML = users
+            .map( (user)=>`<p>${user.name}</p>` )
+            .join(' ')
+    } catch (erreur){
+
+    }
+}
+fetchUsers()
